e4b6d493aa77cc54d7986e3d593bc9f11b27f7e9
Pryme8 <Pryme8@gmail.com>
2018-01-06 20:58:25 -0800

    Implementing Amazon Redirect Page - With Popup

--- wp-content/plugins/woo-multistep-checkout/assets/js/thwmscf-frontend.js
+++ wp-content/plugins/woo-multistep-checkout/assets/js/thwmscf-frontend.js
@@ -13,12 +13,44 @@ function amazon_checkout_redirect() {
     data = { action: 'woozone_woo_cart_amazon_redirect' };
 
     jQuery.post(ajaxurl, data, (response)=>{
-		var page = document.getElementById('primary');
+		
+		/*var page = document.getElementById('primary');
 		page.innerHTML = response;
-        console.log(response);
-		setTimeout(function() {
+        console.log(response);*/
+		
+		
+		
+		function PopupCenter(url, title, w, h) {
+			// Fixes dual-screen position  Most browsers      Firefox
+			var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+			var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+			var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+			var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+			var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+			var top = ((height / 2) - (h / 2)) + dualScreenTop;
+			var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+			
+			newWindow.onload = (e)=>{
+				newWindow.document.body.innerHTML = response;
+				setTimeout(function() {
+					newWindow.document.getElementById("amzRedirect").submit();
+			}, 3000);
+			};
+
+			// Puts focus on the newWindow
+			/*if (window.focus) {
+				newWindow.focus();
+			}*/
+		}
+		
+		PopupCenter('./','Complete Purchase','600','400');  
+		
+		
+		/*setTimeout(function() {
 					document.getElementById("amzRedirect").submit();
-		}, 3000);
+		}, 3000);*/
         // window.location.href = "/checkout-2/"
     });
 
